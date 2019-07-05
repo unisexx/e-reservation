055b75c3e181ee045cf2d029fc2c8a1a523ffed3
Ratasak permchob <unisexx@gmail.com>
2019-07-05 14:26:01 +0700

    updated

--- resources/views/include/_script.blade.php
+++ resources/views/include/_script.blade.php
@@ -13,7 +13,9 @@
 
 <!-- Fonts -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+<link href="https://fonts.googleapis.com/css?family=Sarabun&display=swap" rel="stylesheet">
+
 
 <!-- Styles -->
 <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
